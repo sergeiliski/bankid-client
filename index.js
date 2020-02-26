7fd4d7ef1fcb9f19653e457a793e4b480aa5ccb7
Sergei Liski <sergeiliski@gmail.com>
2020-02-26 01:51:13 +0000

    Major update for bankid client to use new required json endpoints

--- index.js
+++ index.js
@@ -1,234 +1,92 @@
 'use strict'
 
 const fs = require('fs')
-const soap = require('soap')
-const xml2js = require('xml2js')
+const axios = require('axios')
+const https = require('https')
 
 class BankID {
-  constructor(options) {
-    if (!options) {
-      throw new Error('Must include options.');
+
+  constructor(connectionOptions) {
+    if (!connectionOptions) {
+      throw Error('Must include options')
     }
-    this.client = null;
-    this.id = null;
-    this.options = options;
-  }
-
-  connect(personalNumber) {
-    if(!personalNumber) {
-      throw new Error('Must contain personal number.');
+    this.options = {
+      ...connectionOptions
     }
-    const options = {
-      wsdl_options: {
-        cert: fs.readFileSync(this.options.cert),
-        key: fs.readFileSync(this.options.key),
-        passphrase: this.options.passphrase,
-        strictSSL: false
-      }
+    if (!this.options.pfx || !this.options.passphrase) {
+      throw Error('Certificate and passphrase are required')
     }
-    return new Promise((resolve, reject) => {
-      return soap.createClient(this.options.url, options, (err, client) => {
-        if (err) {
-          return reject(new Error(err))
-        } else {
-          this.id = personalNumber
-          this.client = client
-          this.client.setSecurity(new soap.ClientSSLSecurity(
-            this.options.key,
-            this.options.cert,
-            {
-              strictSSL: false,
-              passphrase: this.options.passphrase
-            }
-          ))
-          return resolve(client)
-        }
-      })
-    })
+    this.baseUrl = this.options.baseUrl
+    if (this.baseUrl.substr(-1) != '/') {
+      this.baseUrl += '/'
+    }
+    this.axios = this._createAxiosInstance()
+    this.authUrl = 'auth'
+    this.signUrl = 'sign'
+    this.collectUrl = 'collect'
+    this.cancelUrl = 'cancel'
   }
 
-  authenticate() {
-    let args = { personalNumber: this.id };
-    return new Promise((resolve, reject) => {
-      return this.client.Authenticate(args, (err, result) => {
-        if (err) {
-          const parser = new xml2js.Parser();
-          parser.parseString(err.body, function(err, result) {
-            if (err) {
-              return reject(new Error(err));
-            }
-            if (
-              result &&
-              result['soap:Envelope'] &&
-              result['soap:Envelope']['soap:Body'] &&
-              result['soap:Envelope']['soap:Body'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0]
-            ) {
-              return resolve({
-                error: result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0]
-              });
-            }
-          })
-          return reject(new Error(err));
-        } else {
-          return resolve({
-            orderRef: result.orderRef,
-            autoStartToken: result.autoStartToken
-          });
-        }
-      })
-    })
+  async auth(data) {
+    if (!data.endUserIp || !data.personalNumber) {
+      throw Error('Both user ip and personal number are required')
+    }
+
+    const params = {
+      endUserIp: data.endUserIp,
+      personalNumber: data.personalNumber
+    }
+
+    return await this.axios.post(this.baseUrl.concat(this.authUrl), params)
   }
 
-  cancelAuthenticate() {
-    const args = { personalNumber: this.id };
-    return new Promise((resolve, reject) => {
-      return this.client.Authenticate(args, (err, result) => {
-        if (err) {
-          const parser = new xml2js.Parser();
-          parser.parseString(err.body, function(err, result) {
-            if (err) {
-              return reject(new Error(err));
-            }
-            if (
-              result &&
-              result['soap:Envelope'] &&
-              result['soap:Envelope']['soap:Body'] &&
-              result['soap:Envelope']['soap:Body'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0] === 'ALREADY_IN_PROGRESS'
-            ) {
-              return resolve('AUTH_CANCELLED')
-            }
-          })
-          return reject(new Error(err));
-        } else {
-          return resolve(this.cancelAuthenticate());
-        }
-      })
-    })
+  async sign(data) {
+    if (!data.endUserIp || !data.personalNumber || !data.userVisibleData) {
+      throw Error('User ip, personal number and visible data are required')
+    }
+
+    let params = {
+      endUserIp: data.endUserIp,
+      personalNumber: data.personalNumber,
+      userVisibleData: data.userVisibleData
+    }
+
+    if (data.userNonVisibleData) {
+      params = { ...params, ... { userNonVisibleData: data.userNonVisibleData }}
+    }
+
+    return await this.axios.post(this.baseUrl.concat(this.signUrl), params)
   }
 
-  collect(orderRef) {
-    return new Promise((resolve, reject) => {
-      if (!orderRef || typeof orderRef !== 'string') {
-        return reject('orderRef is required in a string format.');
+  async collect(orderRef) {
+    if (!orderRef) {
+      throw Error('Order reference value is required')
+    }
+    const params = {
+      orderRef
+    }
+    return await this.axios.post(this.baseUrl.concat(this.collectUrl), params)
+  }
+
+  async cancel(orderRef) {
+    if (!orderRef) {
+      throw Error('Order reference value is required')
+    }
+    const params = {
+      orderRef
+    }
+    return await this.axios.post(this.baseUrl.concat(this.cancelUrl), params)
+  }
+
+  _createAxiosInstance() {
+    const ca = fs.readFileSync(this.options.ca, 'utf-8')
+    const pfx = fs.readFileSync(this.options.pfx)
+    const passphrase = this.options.passphrase
+    return axios.create({
+      httpsAgent: new https.Agent({ pfx, passphrase, ca }),
+      headers: {
+        'Content-Type': 'application/json'
       }
-
-      return this.client.Collect(orderRef, (err, result) => {
-        if (err) {
-          const parser = new xml2js.Parser();
-          parser.parseString(err.body, function(err, result) {
-            if (err) {
-              return reject(new Error(err));
-            }
-            if (
-              result &&
-              result['soap:Envelope'] &&
-              result['soap:Envelope']['soap:Body'] &&
-              result['soap:Envelope']['soap:Body'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0]
-            ) {
-              return resolve(result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0]);
-            }
-          })
-          return reject(new Error(err));
-        } else {
-          return resolve(result);
-        }
-      })
-    })
-  }
-
-  sign(data) {
-    return new Promise((resolve, reject) => {
-      if (!data || typeof data !== 'object') {
-        return reject('Signable data is required.');
-      }
-  
-      if (!data.userNonVisibleData || !data.userNonVisibleData) {
-        return reject('Both userNonVisibleData and userVisibleData is required.');
-      }
-  
-      const args = {
-        personalNumber: this.id,
-        userVisibleData: data.userVisibleData,
-        userNonVisibleData: data.userNonVisibleData
-      };
-
-      return this.client.Sign(args, (err, result) => {
-        if (err) {
-          const parser = new xml2js.Parser()
-          parser.parseString(err.body, function(err, result) {
-            if (err) {
-              return reject(new Error(err));
-            }
-            if (
-              result &&
-              result['soap:Envelope'] &&
-              result['soap:Envelope']['soap:Body'] &&
-              result['soap:Envelope']['soap:Body'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0]
-            ) {
-              return resolve({
-                error: result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0]
-              });
-            }
-          })
-          return reject(new Error(err));
-        } else {
-          return resolve({
-            orderRef: result.orderRef,
-            autoStartToken: result.autoStartToken
-          });
-        }
-      })
-    })
-  }
-
-  cancelSign() {
-    const args = {
-      personalNumber: this.id,
-      userVisibleData: Buffer.from('Cancel').toString('base64'),
-      userNonVisibleData: Buffer.from('Cancel').toString('base64')
-    };
-    return new Promise((resolve, reject) => {
-      return this.client.Sign(args, (err, result) => {
-        if (err) {
-          const parser = new xml2js.Parser()
-          parser.parseString(err.body, function(err, result) {
-            if (err) {
-              return reject(new Error(err));
-            }
-            if (
-              result &&
-              result['soap:Envelope'] &&
-              result['soap:Envelope']['soap:Body'] &&
-              result['soap:Envelope']['soap:Body'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'] &&
-              result['soap:Envelope']['soap:Body'][0]['soap:Fault'][0]['faultstring'][0] === 'ALREADY_IN_PROGRESS'
-            ) {
-              return resolve('SIGN_CANCELLED');
-            }
-          })
-          return reject(new Error(err));
-        } else {
-          return resolve(this.cancelSign());
-        }
-      })
     })
   }
 }
